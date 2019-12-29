148d72a09960967887c9c86866ad36ec1447c7aa
Justin Locsei <justin.locsei@gmail.com>
2019-12-28 21:11:28 -0500

    Replace commander with yargs

--- src/cli.ts
+++ src/cli.ts
@@ -1,40 +1,71 @@
-import commander from 'commander';
+import yargs from 'yargs';
 import { mkdirp } from 'fs-extra';
 
-import APIClient from './api-client';
 import { paths } from './environment';
-
-interface CLIOptions {
-
-}
+import { scrape } from './tasks/scrape';
 
 class CLI {
 
-  private options: CLIOptions;
+  private args: string[];
 
   /**
    * Create a new CLI
    */
   constructor(args: string[]) {
-    this.options = this.parseArgs(args);
+    this.args = args;
   }
 
   /**
    * Run the CLI
    */
-  async run(): Promise<void> {
-    await mkdirp(paths.cacheDir);
+  run(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const parser = yargs
+        .option('data-dir', {
+          default: paths.dataDir,
+          describe: 'The directory in which to store processed data',
+          type: 'string'
+        })
+        .option('h', {
+          alias: 'help',
+          describe: 'Show usage',
+          type: 'boolean'
+        })
+        .command(
+          'scrape',
+          'Scrape data from Goodreads',
+          function(y) {
+            return y
+              .option('cache-dir', {
+                default: paths.cacheDir,
+                describe: 'The directory in which to cache API responses',
+                type: 'string'
+              });
+          },
+          async function(args): Promise<void> {
+            if (args.h) {
+              return Promise.resolve();
+            }
 
-    const client = new APIClient({
-      cacheDir: paths.cacheDir
+            await mkdirp(args['cache-dir']);
+
+            return scrape({
+              cacheDir: args['cache-dir'],
+              dataDir: args['data-dir']
+            });
+          }
+        )
+        .demandCommand(1, 'You must select a mode of operation')
+        .strict()
+        .help(false);
+
+      const options = parser.parse(this.args.slice(1));
+
+      if (options.h) {
+        parser.showHelp();
+        resolve();
+      }
     });
-  }
-
-  /**
-   * Convert command-line arguments to options
-   */
-  private parseArgs(args: string[]): CLIOptions {
-    return {};
   }
 
 }
