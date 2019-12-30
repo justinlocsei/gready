3ba33132bd0ba6cc39e8e6483fbf0bb2a7f347f2
Justin Locsei <justin.locsei@gmail.com>
2019-12-30 13:13:33 -0500

    Get the user ID when reading a stored session

--- src/api-client.ts
+++ src/api-client.ts
@@ -297,7 +297,9 @@ export default class APIClient {
     }
 
     const session = await this.loadSession();
+
     this.credentials = session.credentials;
+    this.userID = session.userID;
 
     return this.credentials;
   }
