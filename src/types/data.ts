5ccb8abc7657c08e962b2ea0925eb95c02088b3d
Justin Locsei <justin.locsei@gmail.com>
2020-01-07 13:08:48 -0500

    Expand the data types

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,5 +1,19 @@
+export interface Author {
+  id: AuthorID;
+  name: string;
+}
+
+type AuthorID = string;
+
 export interface Book {
+  authors: Author[];
+  averageRating?: number;
+  categories: Category[];
   id: BookID;
+  publisher: string;
+  similarBooks: BookID[];
+  totalRatings: number;
+  workID: WorkID;
 }
 
 export type BookID = string;
@@ -9,4 +23,11 @@ export interface BookReview {
   rating: number;
 }
 
+export interface Category {
+  count: number;
+  name: string;
+}
+
 export type UserID = string;
+
+type WorkID = string;
