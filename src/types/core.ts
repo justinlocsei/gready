53393ec3777c109fc9b2d27146d6968b900f88ca
Justin Locsei <justin.locsei@gmail.com>
2020-01-07 12:39:27 -0500

    Remove unused types

--- src/types/core.ts
+++
@@ -1,4 +0,0 @@
-type JSONArray = JSONSerializable[];
-type JSONObject = { [member: string]: JSONSerializable; };
-type JSONPrimitive = boolean | null | number | string;
-export type JSONSerializable = JSONPrimitive | JSONObject | JSONArray;
