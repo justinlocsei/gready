210eae98474440ab4283612a7ffa6dbd250d5bef
Justin Locsei <justin.locsei@gmail.com>
2020-01-07 13:48:00 -0500

    Add a function to normalize a string

--- src/data.ts
+++ src/data.ts
@@ -6,6 +6,15 @@ export function ensureArray<T>(value: T | T[]): T[] {
 }
 
 /**
+ * Remove non-essential information from a string
+ */
+export function normalizeString(value: string): string {
+  return value
+    .trim()
+    .replace(/\s{2,}/g, ' ');
+}
+
+/**
  * A function that should never be called
  */
 export function unreachable(param: never) {}
