cd00eff61b13ba1ff58c3b0f56b080a84749af99
Justin Locsei <justin.locsei@gmail.com>
2020-01-07 13:09:21 -0500

    Make the definition of schemas for API types easier

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,20 +1,29 @@
 import Ajv from 'ajv';
 
-interface APIType<T> {
+import { formatJSON } from '../serialization';
+
+interface APITypeSchema<T> {
   conform: (data: unknown) => T;
   name: string;
 }
 
 type JSONSchema =
+  | { anyOf: JSONSchema[]; }
   | { type: 'array'; items: JSONSchema; }
   | { type: 'number'; }
   | { type: 'object'; properties?: Record<string, JSONSchema>; required?: string[]; }
   | { type: 'string'; }
 
+type ExtractArrayType<T> = T extends (infer U)[] ? U : T;
+type ExtractSchemaType<T extends APITypeSchema<any>> = ReturnType<T['conform']>;
+type OneOrMore<T> = T | T[];
+
+export type ResponseBody = Record<string, any>;
+
 /**
- * Define a type that can be used to validate API data
+ * Define a schema for a type that can be used to validate API data
  */
-function defineType<T>(name: string, schema: Record<string, JSONSchema>): APIType<T> {
+function defineSchema<T>(name: string, schema: Record<string, JSONSchema>): APITypeSchema<T> {
   const jsonSchema: JSONSchema = {
     properties: schema,
     required: Object.keys(schema),
@@ -44,39 +53,110 @@ function conformToType<T>(typeName: string, schema: JSONSchema, data: unknown): 
   let error: string;
 
   if (errors && errors.length) {
-    error = errors[0].message || JSON.stringify(errors[0].params, null, 2);
+    error = errors[0].message || formatJSON(errors[0].params);
     error = `${errors[0].dataPath} ${error}`;
   } else {
     error = 'unknown validation error';
   }
 
-  throw new Error(`Invalid ${typeName}: ${error}\n${JSON.stringify(data, null, 2)}`);
+  throw new Error(`Invalid ${typeName}: ${error}\n${formatJSON(data)}`);
 }
 
 /**
  * Extract the body from an API response
  */
 export function extractResponseBody(response: unknown): ResponseBody {
-  return Response.conform(response).GoodreadsResponse;
+  return ResponseSchema.conform(response).GoodreadsResponse;
 }
 
-export type ResponseBody = Record<string, any>;
-export type ResponseType<T extends APIType<any>> = ReturnType<T['conform']>;
+const T = {
+  array: (items: JSONSchema): JSONSchema => ({
+    items ,
+    type: 'array'
+  }),
+  number: (): JSONSchema => ({
+    type: 'number'
+  }),
+  object: <T extends Record<string, JSONSchema>>(props?: T, optional: (keyof T)[] = []): JSONSchema => ({
+    properties: props,
+    required: props && Object.keys(props).filter(k => optional.includes(k)),
+    type: 'object'
+  }),
+  oneOrMore: (value: JSONSchema) => T.union([
+    value,
+    T.array(value)
+  ]),
+  string: (): JSONSchema => ({
+    type: 'string'
+  }),
+  union: (members: JSONSchema[]): JSONSchema => ({
+    anyOf: members
+  })
+};
 
-export const BookInfo = defineType<{
+export const BookInfoSchema = defineSchema<{
   book: {
+    authors: {
+      author: OneOrMore<{
+        id: string;
+        name: string;
+      }>;
+    };
     id: string;
+    popular_shelves: {
+      shelf: {
+        $: {
+          count: string;
+          name: string;
+        };
+      }[];
+    };
+    publisher: string;
+    similar_books: {
+      book: {
+        id: string;
+      }[];
+    };
+    work: {
+      id: { _: string; };
+      ratings_count: { _: string; };
+      ratings_sum: { _: string; };
+    };
   };
 }>('book info', {
-  book: {
-    type: 'object',
-    properties: {
-      id: { type: 'string' }
-    }
-  }
+  book: T.object({
+    authors: T.object({
+      author: T.oneOrMore(T.object({
+        id: T.string(),
+        name: T.string()
+      }))
+    }),
+    id: T.string(),
+    popular_shelves: T.object({
+      shelf: T.array(T.object({
+        $: T.object({
+          count: T.string(),
+          name: T.string()
+        })
+      }))
+    }),
+    publisher: T.string(),
+    similar_books: T.object({
+      book: T.array(T.object({
+        id: T.string()
+      }))
+    }),
+    work: T.object({
+      id: T.object({ _: T.string() }),
+      ratings_count: T.object({ _: T.string() }),
+      ratings_sum: T.object({ _: T.string() })
+    })
+  })
 });
 
-export const BookReviews = defineType<{
+export type BookInfo = ExtractSchemaType<typeof BookInfoSchema>['book'];
+
+export const BookReviewsSchema = defineSchema<{
   reviews: {
     $: {
       end: string;
@@ -86,70 +166,49 @@ export const BookReviews = defineType<{
     review: {
       book: {
         id: {
-          _: 'string';
+          _: string;
         };
       };
       rating: string;
     }[];
   };
 }>('book reviews', {
-  reviews: {
-    type: 'object',
-    properties: {
-      $: {
-        type: 'object',
-        properties: {
-          end: { type: 'string' },
-          start: { type: 'string' },
-          total: { type: 'string' }
-        }
-      },
-      review: {
-        type: 'array',
-        items: {
-          type: 'object',
-          properties: {
-            book: {
-              type: 'object',
-              properties: {
-                id: {
-                  type: 'object',
-                  properties: {
-                    _: { type: 'string' }
-                  }
-                }
-              }
-            },
-            rating: { type: 'string' }
-          }
-        }
-      }
-    }
-  }
+  reviews: T.object({
+    $: T.object({
+      end: T.string(),
+      start: T.string(),
+      total: T.string()
+    }),
+    review: T.array(T.object({
+      book: T.object({
+        id: T.object({
+          _: T.string()
+        })
+      }),
+      rating: T.string()
+    }))
+  })
 });
 
-const Response = defineType<{
+export type BookReview = ExtractArrayType<BookReviews['reviews']['review']>;
+export type BookReviews = ExtractSchemaType<typeof BookReviewsSchema>;
+
+const ResponseSchema = defineSchema<{
   GoodreadsResponse: ResponseBody;
 }>('response', {
-  GoodreadsResponse: { type: 'object' }
+  GoodreadsResponse: T.object()
 });
 
-export const UserResponse = defineType<{
+export const UserResponseSchema = defineSchema<{
   user: {
     $: {
       id: string;
     };
   };
 }>('user data', {
-  user: {
-    type: 'object',
-    properties: {
-      $: {
-        type: 'object',
-        properties: {
-          id: { type: 'string' }
-        }
-      }
-    }
-  }
+  user: T.object({
+    $: T.object({
+      id: T.string()
+    })
+  })
 });
