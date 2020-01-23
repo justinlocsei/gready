7a8f4de43f1d29d8481c298f1a125c74c71b918a
Justin Locsei <justin.locsei@gmail.com>
2020-01-22 20:18:14 -0500

    Add a test helper for running a local server

--- tests/helpers/requests.ts
+++ tests/helpers/requests.ts
@@ -1,4 +1,5 @@
 import FSPersister from '@pollyjs/persister-fs';
+import http from 'http';
 import NodeHTTPAdapter from '@pollyjs/adapter-node-http';
 import { Polly } from '@pollyjs/core';
 
@@ -41,4 +42,44 @@ export async function useNetworkFixture(
   await runTest();
 
   await polly.stop();
+}
+
+/**
+ * Use a local server that responds with the given data
+ */
+export function useServer(
+  status: number,
+  headers: Record<string, string>,
+  provideBody: (request: http.IncomingMessage) => string,
+  runTest: (url: string) => Promise<void>
+): Promise<void> {
+  let error: Error | undefined;
+
+  return new Promise(function(resolve, reject) {
+    const server = http.createServer(function(request, response) {
+      response.writeHead(status, headers);
+      response.end(provideBody(request));
+    });
+
+    server.on('close', function() {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+
+    server.listen(function() {
+      const address = server.address();
+
+      if (address && typeof address !== 'string') {
+        runTest(`http://127.0.0.1:${address.port}`)
+          .catch(e => error = e)
+          .then(() => server.close());
+      } else {
+        error = new Error(`Invalid server address: ${address}`);
+        server.close();
+      }
+    });
+  });
 }
