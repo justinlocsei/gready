4b9df7f17f2c31e0e1745a88e332197b5d4c049f
Justin Locsei <justin.locsei@gmail.com>
2020-01-22 20:18:24 -0500

    Use a local server for network tests

--- tests/network.test.ts
+++ tests/network.test.ts
@@ -1,32 +1,65 @@
-import nock from 'nock';
-
 import assert from './helpers/assert';
 import { makeGetRequest } from '../src/network';
+import { useServer } from './helpers/requests';
 
 describe('network', function() {
 
   describe('makeGetRequest', function() {
 
-    it('returns the text of a response', async function() {
-      nock('http://example.com')
-        .get('/test')
-        .reply(200, 'response');
-
-      assert.equal(
-        await makeGetRequest('http://example.com/test'),
-        'response'
-      );
+    it('returns the text of a successful response', function() {
+      return useServer(
+        200,
+        {},
+        () => 'Response',
+        async function(url) {
+          assert.equal(
+            await makeGetRequest(url),
+            'Response'
+          );
+        });
     });
 
-    it('handles errors', async function() {
-      nock('http://example.com')
-        .get('/test')
-        .reply(500);
+    it('returns the text of an XML response', function() {
+      return useServer(
+        200,
+        { 'Content-Type': 'application/xml' },
+        () => '<Testing></Testing>',
+        async function(url) {
+          assert.equal(
+            await makeGetRequest(url),
+            '<Testing></Testing>'
+          );
+        });
+    });
 
-      assert.isRejected(
-        makeGetRequest('http://example.com/test'),
-        /example\.com\/test failed:/
-      );
+    it('supports setting a payload', function() {
+      return useServer(
+        200,
+        {},
+        function(request) {
+          return request.url
+            ? request.url.replace(/.*\?/, '')
+            : '';
+        },
+        async function(url) {
+          assert.equal(
+            await makeGetRequest(url, { key: 'value' }),
+            'key=value'
+          );
+        });
+    });
+
+    it('handles errors', function() {
+      return useServer(
+        404,
+        {},
+        () => 'Invalid',
+        async function(url) {
+          await assert.isRejected(
+            makeGetRequest(url),
+            /failed/
+          );
+        });
     });
 
   });
