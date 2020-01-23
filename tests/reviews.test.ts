7cb6a88721baa5e38ac9bc012a3fde3558dd5522
Justin Locsei <justin.locsei@gmail.com>
2020-01-23 08:14:27 -0500

    Specify excluded headers in the reviews tests

--- tests/reviews.test.ts
+++ tests/reviews.test.ts
@@ -9,15 +9,26 @@ describe('reviews', function() {
     this.slow(9000);
     this.timeout(10000);
 
+    function withFixture(name: string, runTest: () => Promise<void>) {
+      return useNetworkFixture(name, runTest, {
+        removeHeaders: [
+          'set-cookie',
+          'x-amz-rid',
+          'x-request-id',
+          'x-runtime'
+        ]
+      });
+    }
+
     it('returns an empty list for a book without reviews', async function() {
-      await useNetworkFixture('reviews/empty', async function() {
+      await withFixture('reviews/empty', async function() {
         const reviews = await findPartialReviewsForBook('1');
         assert.deepEqual(reviews, []);
       });
     });
 
     it('returns the top reviews for a book by default', async function() {
-      await useNetworkFixture('reviews/default', async function() {
+      await withFixture('reviews/default', async function() {
         const reviews = await findPartialReviewsForBook('0439785960');
 
         assert.equal(reviews.length, 10);
@@ -29,7 +40,7 @@ describe('reviews', function() {
     });
 
     it('can limit the number of returned reviews', async function() {
-      await useNetworkFixture('reviews/limit', async function() {
+      await withFixture('reviews/limit', async function() {
         const reviews = await findPartialReviewsForBook('0439785960', { limit: 5 });
 
         assert.equal(reviews.length, 5);
@@ -37,7 +48,7 @@ describe('reviews', function() {
     });
 
     it('can filter the reviews by rating', async function() {
-      await useNetworkFixture('reviews/rating', async function() {
+      await withFixture('reviews/rating', async function() {
         const four = await findPartialReviewsForBook('0439785960', { limit: 5, rating: 4 });
         const five = await findPartialReviewsForBook('0439785960', { limit: 5, rating: 5 });
 
