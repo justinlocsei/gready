dfc4092e41183070542b8337c1956f62e404a0f1
Justin Locsei <justin.locsei@gmail.com>
2020-01-24 21:06:18 -0500

    Add a test helper for stubbing objects

--- tests/helpers/mocking.ts
+++ tests/helpers/mocking.ts
@@ -1,3 +1,5 @@
+import sinon from 'sinon';
+
 /**
  * Freeze time inside of a function
  */
@@ -11,4 +13,15 @@ export function freezeTime(timestamp: number, runTest: () => any): void {
   } finally {
     Date.now = originalDateNow;
   }
+}
+
+/**
+ * Replace a method on an object with a custom implementation
+ */
+export function override<T, K extends keyof T>(
+  object: T,
+  method: K,
+  impl: T[K] extends (...args: infer U) => infer V ? (...args: U) => V : never
+): void {
+  sinon.stub(object, method).callsFake(impl);
 }
