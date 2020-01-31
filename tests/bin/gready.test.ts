90cc3cc173fb425f194c2b4ec9f08783bea145da
Justin Locsei <justin.locsei@gmail.com>
2020-01-31 10:22:58 -0500

    Add tests for the gready bin

---
+++ tests/bin/gready.test.ts
@@ -0,0 +1,29 @@
+import * as gready from '../../src/gready';
+import * as system from '../../src/system';
+import assert from '../helpers/assert';
+import { allowOverrides } from '../helpers/mocking';
+import { run } from '../../src/bin/gready';
+
+describe('bin/gready', function() {
+
+  const { expectAssertions, override } = allowOverrides(this);
+
+  it('runs the gready CLI with normalized arguments', async function() {
+    const plan = expectAssertions(1);
+
+    override(system, 'getArgs', () => ['alfa', 'bravo']);
+
+    override(gready, 'runCLI', async function({ args }) {
+      plan.assert(function() {
+        assert.deepEqual(args, ['alfa', 'bravo']);
+      });
+
+      return true;
+    });
+
+    await run();
+
+    plan.verify();
+  });
+
+});
