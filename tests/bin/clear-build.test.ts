be90d08eb633e19176465e3b7735424dd3a961d5
Justin Locsei <justin.locsei@gmail.com>
2020-01-30 19:51:53 -0500

    Add tests for the clearing scripts

---
+++ tests/bin/clear-build.test.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import { readFileSync } from 'graceful-fs';
+
+import * as scripts from '../../src/scripts';
+import assert from '../helpers/assert';
+import { allowOverrides } from '../helpers/mocking';
+import { paths } from '../../src/environment';
+import { run } from '../../src/bin/clear-build';
+
+describe('bin/clear-build', function() {
+
+  const { expectAssertions, stub } = allowOverrides(this);
+
+  it('clears transient build paths', async function() {
+    const ignoredPaths = readFileSync(paths.gitignore, 'utf8')
+      .split('\n')
+      .filter(l => l.startsWith('/'))
+      .map(l => path.join(paths.rootDir, l));
+
+    const plan = expectAssertions(1);
+
+    stub(scripts, 'removePaths', async function(title, removePaths) {
+      plan.assert(function() {
+        assert.isNotEmpty(removePaths);
+
+        removePaths.forEach(function(removePath) {
+          assert.include(ignoredPaths, removePath + '/');
+        });
+      });
+    });
+
+    await run();
+
+    plan.verify();
+  });
+
+});
---
+++ tests/bin/clear-test-fixtures.test.ts
@@ -0,0 +1,35 @@
+import path from 'path'
+
+import * as scripts from '../../src/scripts';
+import assert from '../helpers/assert';
+import { allowOverrides } from '../helpers/mocking';
+import { paths } from '../../src/environment';
+import { run } from '../../src/bin/clear-test-fixtures';
+
+describe('bin/clear-test-fixtures', function() {
+
+  const { expectAssertions, stub } = allowOverrides(this);
+
+  it('clears test directories', async function() {
+    const plan = expectAssertions(1);
+
+    stub(scripts, 'removePaths', async function(title, removePaths) {
+      plan.assert(function() {
+        assert.isNotEmpty(removePaths);
+
+        removePaths.forEach(function(removePath) {
+          assert.match(
+            path.relative(paths.testsDir, removePath),
+            /^[a-z]/,
+            `${removePath} was not under the tests directory`
+          );
+        });
+      });
+    });
+
+    await run();
+
+    plan.verify();
+  });
+
+});
